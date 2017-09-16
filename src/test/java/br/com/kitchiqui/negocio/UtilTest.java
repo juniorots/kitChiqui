11e132a751e83401da8a2ed2f3c217c0b4ea9fe1
juniorots <juniorots@gmail.com>
2017-09-16 20:20:49 -0300

    atualizando o projeto

---
+++ src/test/java/br/com/kitchiqui/negocio/UtilTest.java
@@ -0,0 +1,72 @@
+/**
+ * Vendas de Kits chiques.
+ * 
+ * @author Jose Alves
+ */
+package br.com.kitchiqui.negocio;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import lombok.Cleanup;
+
+public class UtilTest {
+	
+	public UtilTest() {		
+	}
+	
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void model() {
+        
+    }
+    
+//    @Test
+    public void tratarPagina() {
+    	 @Cleanup
+         final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("databaseDefault");
+
+         @Cleanup
+         final EntityManager entityManager = entityManagerFactory.createEntityManager();
+         entityManager.getTransaction().begin();
+         
+         /*
+          * 	TEMPLATE INSERCAO DE OBJETO NA BASE.
+         suario usuario = new Usuario();    
+         UsuarioDAO dao = new UsuarioDAO(entityManager);
+         
+//         usuario.setEmail("juniorots@gmail.com");
+//         usuario.setSenha("12345");
+//         usuario.setReceberEmail("S");
+         
+         Usuario usInserido = dao.insert(usuario);
+         
+         entityManager.getTransaction().commit();
+
+         List<Usuario> usuarios = dao.selectAll();
+         assertNotNull(usInserido.getId());
+         */
+    }
+}
